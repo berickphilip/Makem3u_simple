6b53b7a489056309c91ccc2f36310022c9cfd220
HonestMike <miyamoto.fan.mike@gmail.com>
2024-08-19 00:41:43 +0100

    Update Makem3u.sh
    fixed a bug that was incorrectly grabbing the root folder name into the m3u file

--- Makem3u.sh
+++ Makem3u.sh
@@ -7,14 +7,9 @@ if [ ${#files[@]} -eq 0 ]; then
     exit 1
 fi
 
-# Function to get the base name (without disc number and extension)
+# Function to get the base game name (without disc number and extension)
 get_basename() {
-    echo "$1" | sed -E 's/(.*) \([Dd]isc [0-9]+\)\.(chd|rvz)/\1/' | sed 's/\.\.\.$//' | sed 's/[[:space:]]*$//'
-}
-
-# Function to remove the file extension from .chd or .rvz files
-remove_extension() {
-    echo "$1" | sed 's/\.[cC][hH][dD]$//' | sed 's/\.[rR][vV][zZ]$//'
+    echo "$1" | sed -E 's/ \([Dd]isc [0-9]+\)\.(chd|rvz)$//' | sed 's/\.\.\.$//' | sed 's/[[:space:]]*$//'
 }
 
 # Process each .chd or .rvz file and group by base name
@@ -29,37 +24,37 @@ done
 for game in "${!games[@]}"; do
     # Get the list of discs for the current game
     disk_list=$(echo -e "${games[$game]}" | sort)
-    
+
     # Count the number of discs
     num_disks=$(echo "$disk_list" | wc -l)
-    
+
     echo "Game: '$game', Number of Discs: $num_disks"  # Debugging output
-    
-    # Clean up game name for directory creation (preserve brackets and other characters)
-    clean_game_name=$(remove_extension "$game")
-    
+
+    # Clean up game name for directory and file creation
+    clean_game_name=$(echo "$game" | sed 's/[[:space:]]$//' | sed 's/\.[cC][hH][dD]$//' | sed 's/\.[rR][vV][zZ]$//')
+
     # Add the .m3u extension to the folder name
     folder_name="${clean_game_name}.m3u"
-    
+
     # Create a folder for the game with the .m3u extension
     mkdir -p "$folder_name"
-    
+
     # Move the discs into the game's folder
     echo "$disk_list" | while IFS= read -r disc; do
         echo "Moving '$disc' to '$folder_name/'"  # Debugging output
         mv -- "$disc" "$folder_name/"
     done
-    
-    # Only create .m3u file if there are more than one disc
+
+    # Only create .m3u file if there is more than one disc
     if [ "$num_disks" -gt 1 ]; then
-        # Create the .m3u file with the correct name
+        # Create the .m3u file with the same name as the folder
         m3u_file="${folder_name}/${clean_game_name}.m3u"
-        
-        # Write the list of discs to the .m3u file without the file extensions
-        echo "$disk_list" | sed "s#^#$folder_name/#" | while IFS= read -r line; do
-            echo "$(remove_extension "$line")" >> "$m3u_file"
+
+        # Write the full filenames (with extensions) to the .m3u file
+        echo "$disk_list" | while IFS= read -r line; do
+            echo "$(basename "$line")" >> "$m3u_file"
         done
-        
+
         echo "Created $m3u_file with the following discs:"
         cat "$m3u_file"
     else
