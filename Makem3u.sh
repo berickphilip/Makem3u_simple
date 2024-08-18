b1b0592ffe62e620ab4f1f20250688d84f68b0eb
HonestMike <miyamoto.fan.mike@gmail.com>
2024-08-18 20:11:41 +0100

    Update Makem3u.sh
    fixed issue with double appending .m3u after folder creation.

--- Makem3u.sh
+++ Makem3u.sh
@@ -24,34 +24,35 @@ done
 for game in "${!games[@]}"; do
     # Get the list of discs for the current game
     disk_list=$(echo -e "${games[$game]}" | sort)
-
+    
     # Count the number of discs
     num_disks=$(echo "$disk_list" | wc -l)
-
+    
     echo "Game: '$game', Number of Discs: $num_disks"  # Debugging output
-
+    
     # Clean up game name for directory creation
     clean_game_name=$(echo "$game" | sed 's/[^a-zA-Z0-9 ]/_/g' | sed 's/[[:space:]]*$//')
-
+    
     # Add the .m3u extension to the folder name
     folder_name="${clean_game_name}.m3u"
-
+    
     # Create a folder for the game with .m3u extension
     mkdir -p "$folder_name"
-
+    
     # Move the discs into the game's folder
     echo "$disk_list" | while IFS= read -r disc; do
         echo "Moving '$disc' to '$folder_name/'"  # Debugging output
         mv -- "$disc" "$folder_name/"
     done
-
+    
     # Only create .m3u file if there are more than one disc
     if [ "$num_disks" -gt 1 ]; then
-        m3u_file="${folder_name}/${folder_name}.m3u"
-
+        # Create the .m3u file without adding the extension again
+        m3u_file="${folder_name}/${clean_game_name}.m3u"
+        
         # Write the list of discs to the .m3u file
         echo "$disk_list" | sed "s#^#$folder_name/#" > "$m3u_file"
-
+        
         echo "Created $m3u_file with the following discs:"
         cat "$m3u_file"
     else
